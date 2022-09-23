e5440680a8f2149b21dca98a5f2045fc1d505732
Antonio Eduardo Garcia Moreira <tonyeg2020@gmail.com>
2022-09-23 18:47:28 -0300

    FINALIZADO

---
+++ apps/usuarios/urls.py
@@ -0,0 +1,11 @@
+from unicodedata import name
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+    path('cadastro', views.cadastro, name='cadastro'),
+    path('dashboard', views.dashboard, name='dashboard'),
+]
